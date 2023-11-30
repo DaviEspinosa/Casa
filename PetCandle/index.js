0f371f203f75a59b5a2c67c300bf478953cc525f
DaviEspinosa <davipedrosa037@gmail.com>
2023-11-30 20:29:00 -0300

    OK

--- PetCandle/index.js
+++ PetCandle/index.js
@@ -3,15 +3,16 @@ const BtnMissao = document.getElementById('btn-Missao');
 const BtnVisao = document.getElementById('btn-Visao');
 const textMissao = document.querySelector('.text-Missao');
 const textVisao = document.querySelector('.text-Visao');
+const DivNossoAlvo = document.querySelector('.Nossa-Missao-Visao');
 
 BtnMissao.addEventListener("click", ()=>{
+
+    DivNossoAlvo.style.backgroundColor = 'rgb(233, 233, 233)';
+
     BtnMissao.style.backgroundColor = 'rgb(22, 157, 22)';
-    BtnMissao.style.color = 'white'; 
-    BtnMissao.style.padding = '10px';
+    BtnMissao.style.padding = '10px 40px 10px 40px';
     BtnMissao.style.fontSize = '12px';
 
-    BtnVisao.style.backgroundColor = 'rgb(193, 193, 193)';
-    BtnVisao.style.color = 'black'; 
     BtnVisao.style.padding = '7px';
     BtnVisao.style.fontSize = '10px';
 
@@ -22,13 +23,12 @@ BtnMissao.addEventListener("click", ()=>{
 
 BtnVisao.addEventListener("click", ()=>{
 
-    BtnVisao.style.backgroundColor = 'rgb(22, 157, 22)';
-    BtnVisao.style.color = 'white'; 
-    BtnVisao.style.padding = '10px';
+    DivNossoAlvo.style.backgroundColor = 'rgb(18, 78, 41)'
+
+    BtnVisao.style.backgroundColor = '#ff8e15';
+    BtnVisao.style.padding = '10px 40px 10px 40px';
     BtnVisao.style.fontSize = '12px';
 
-    BtnMissao.style.backgroundColor = 'rgb(193, 193, 193)';
-    BtnMissao.style.color = 'black';
     BtnMissao.style.padding = '7px';
     BtnMissao.style.fontSize = '10px';
 
