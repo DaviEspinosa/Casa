f690af25d335cdfa1a5ca16fe99c979701219960
DaviEspinosa <davipedrosa037@gmail.com>
2023-12-03 15:53:26 -0300

    função do carrossel quase pronta

--- PetCandle/index.js
+++ PetCandle/index.js
@@ -37,3 +37,37 @@ BtnVisao.addEventListener("click", ()=>{
 })
 
 //----------=========CARROSSEL===========--------
+var prevBtn = document.getElementById('prevItem');
+var nextBtn = document.getElementById('nextItem');
+var slide = document.getElementById('card-slide');
+let currentPosition = 0;
+function moveSlide(direction) {
+  const cardWidth = 200; // 200px card width
+  currentPosition += direction * cardWidth;
+  slide.style.transform = `translateX(${-currentPosition}px)`;
+}
+prevBtn.addEventListener('click', () => {
+  moveSlide(1); // Move para a direita
+});
+nextBtn.addEventListener('click', () => {
+  moveSlide(-1); // Move para a esquerda
+});
+
+var prevBtn2 = document.getElementById('prevItem2');
+var nextBtn2 = document.getElementById('nextItem2');
+var slide2 = document.getElementById('card-slide2');
+function moveSlide2(direction) {
+
+  var cardWidth = 200; // 200px card width
+  currentPosition += direction * cardWidth;
+  slide2.style.transform = `translateX(${-currentPosition}px)`;
+}
+
+prevBtn2.addEventListener('click', () => {
+  moveSlide2(1); // Move para a direita
+
+});
+
+nextBtn2.addEventListener('click', () => {
+  moveSlide2(-1); // Move para a esquerda
+});
